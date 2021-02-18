d3a42f033ce874073b3f64c57df289e6191141c2
Jacob Lopez <jacoblopez757@gmail.com>
2021-02-18 11:31:17 -0800

    Final version of eval; no errors and no leaks at time of local testing

--- cPostfixCalcEval.c
+++ cPostfixCalcEval.c
@@ -26,6 +26,9 @@ long eval(const char *s) {
 	count[0] = 0;
 
 	if(currentString == NULL){
+		free(count);
+		free(pval);
+		free(pOp);
 		fatalError("Inputted string is empty.");
 	}
 
@@ -33,17 +36,32 @@ long eval(const char *s) {
 
 		if(tokenType(currentString) == TOK_INT){
 			currentString = consumeInt(currentString, pval);
+			if(count[0] == MAX_STACK){
+				free(count);
+				free(pval);
+				free(pOp);
+				fatalError("Too many elements in the stack currently.");
+			}
 			stackPush(numberStack, count, pval[0]);	
 		}
 	
 		else if(tokenType(currentString) == TOK_OP){
 			currentString = consumeOp(currentString, pOp);
+			if(count[0] < 2){
+				free(count);
+				free(pval);
+				free(pOp);
+				fatalError("Not enough elements in stack for operation.");
+			}
 			right = stackPop(numberStack, count);
 			left = stackPop(numberStack, count);
 			stackPush(numberStack, count, evalOp(pOp[0], left, right));
 		}
 	
 		else if(tokenType(currentString) == TOK_UNKNOWN){
+			free(count);
+			free(pval);
+			free(pOp);
 			fatalError("Inputted an invalid string.");
 		}
 
@@ -51,14 +69,24 @@ long eval(const char *s) {
 	}
 
 	if(count[0] == 1){
-		return stackPop(numberStack, count);
+		long answer = stackPop(numberStack, count);
+		free(count);
+		free(pval);
+		free(pOp);	
+		return answer;
 	}
 
 	else if(count[0] > 1){
+		free(count);
+		free(pval);
+		free(pOp);
 		fatalError("Too many items in the stack.");
 	}
 
 	else if(count[0] == 0){
+		free(count);
+		free(pval);
+		free(pOp);
 		fatalError("No items in the stack.");
 	}
 }
